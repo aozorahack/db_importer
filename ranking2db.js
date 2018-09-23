df5f7f56e6d732280424974ea5e298b024d3eac1
Ken Sato <ksato9700@gmail.com>
2018-09-23 11:52:15 +0900

    list ranking pages by reading the ranking top page

--- ranking2db.js
+++ ranking2db.js
@@ -1,5 +1,6 @@
 const Crawler = require('crawler');
 const rp = require('request-promise');
+const Bottleneck = require('bottleneck');
 
 require('dotenv').config();
 
@@ -10,6 +11,8 @@ const mongodb_credential = process.env.AOZORA_MONGODB_CREDENTIAL || '';
 const mongodb_host = process.env.AOZORA_MONGODB_HOST || 'localhost';
 const mongodb_port = process.env.AOZORA_MONGODB_PORT || '27017';
 const mongo_url = `mongodb://${mongodb_credential}${mongodb_host}:${mongodb_port}/aozora`;
+
+const base_url = 'https://www.aozora.gr.jp/access_ranking/'
 
 const re = /.*card(\d+).html/;
 
@@ -48,7 +51,7 @@ const _do_process_response = async (client, res) => {
       console.log(`${res.options.year_month}_${res.options.type}: updated ${result.upsertedCount} entries`); // eslint-disable-line no-console
       return collection.createIndex({year_month: 1, book_id: 1}, {name: 'index'});
     }).catch((error) => {
-      console.error(error);
+      console.error('error', error);
     });
   });
 };
@@ -56,6 +59,7 @@ const _do_process_response = async (client, res) => {
 const process_response = (error, res, done) => {
   if (error) {
     console.error(error);
+    done();
   } else {
     _do_process_response(res.options.client, res).then(()=> {
       done();
@@ -63,49 +67,50 @@ const process_response = (error, res, done) => {
   }
 };
 
-const ymfmt = '%4d_%02d';
-
-const run = () => {
-
-  mongodb.MongoClient.connect(mongo_url, {useNewUrlParser: true}).then((client) => {
-    const etags = client.db().collection('ranking_etags');
-    const crawler = new Crawler({
-      callback: process_response,
-      retries: 0,
-      userAgent: 'Mozilla/5.0',
-      client: client
-    });
-
-    /*
-      url = 'https://www.aozora.gr.jp/access_ranking/2018_08_xhtml.html';
-    */
-
+const process_response_top = (error, res, done) => {
+  const client = res.options.client;
+  const crawler = res.options.crawler;
+  if (error) {
+    console.error(error);
+    done();
+  } else {
     let promises = [];
-    for(let year = 2009; year <= (new Date()).getFullYear(); year++) {
-    // for(let year = 2009; year <= 2009; year++) {
-      for(let month = 1; month <= 12; month++) {
-      // for(let month = 1; month <= 1; month++) {
-        for(let type of ['xhtml', 'txt']) {
-        // for(let type of ['xhtml']) {
-          const year_month = sprintf(ymfmt, year, month);
-          const year_month_type = `${year_month}_${type}`;
-          const url = `https://www.aozora.gr.jp/access_ranking/${year_month_type}.html`;
+    const limiter = new Bottleneck({
+      maxConcurrent: 3,
+      minTime: 500
+    });
+    res.$('a').map((idx, a)=>{
+      const hrefre = /(\d+)_(\d+)_(.+)\.html/.exec(res.$(a).attr('href'));
+      if(hrefre) {
+        const year_month = `${hrefre[1]}_${hrefre[2]}`
+        const type = hrefre[3]
+        const year_month_type = `${year_month}_${type}`
+        const url = base_url + hrefre.input
+        // const url = 'https://www.aozora.gr.jp/access_ranking/2018_08_xhtml.html';
 
-          promises.push(etags.findOne({year_month_type: year_month_type}).then((entry)=>{
+        const etags = client.db().collection('ranking_etags');
+        promises.push(limiter.schedule(()=> {
+          return etags.findOne({year_month_type: year_month_type}).then((entry)=>{
             let options = {
               url: url,
               headers: {
                 'User-Agent': 'Mozilla/5.0'
+              },
+              forever: true,
+              pool: {
+                maxSockets: 1
               }
             };
             if (entry) {
               options.headers['If-None-Match'] = entry.etag;
             }
-            return rp.head(options);
-
+            // console.log('>', year_month,type);
+            return rp.head(options)
           }).then((header)=>{
+            // console.log('<', year_month,type);
             crawler.queue({
               url: url,
+              callback: process_response,
               year_month: year_month,
               type: type
             });
@@ -117,14 +122,14 @@ const run = () => {
             console.log(`Updated etag: ${result.ops[0].year_month_type}`); // eslint-disable-line no-console
             return Promise.resolve();
           }).catch((error) => {
+            // console.log('<', year_month,type);
             if(error.statusCode != 304 && error.statusCode != 404) {
               console.error(error);
             }
             return Promise.resolve();
-          }));
-        }
+          })}));
       }
-    }
+    });
     Promise.all(promises).then(()=> {
       if (crawler.queueSize > 0) {
         crawler.on('drain', () => {
@@ -133,9 +138,26 @@ const run = () => {
       } else {
         client.close();
       }
+      done();
     });
-  }).catch((error)=> {
-    console.error('error', error);
+  }
+};
+
+const ymfmt = '%4d_%02d';
+
+const run = () => {
+
+  mongodb.MongoClient.connect(mongo_url, {useNewUrlParser: true}).then((client) => {
+    const crawler = new Crawler({
+      userAgent: 'Mozilla/5.0',
+      client: client
+    });
+
+    crawler.queue({
+      url: base_url,
+      callback: process_response_top,
+      crawler: crawler
+    })
   });
 };
 
